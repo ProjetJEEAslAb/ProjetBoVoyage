df1e4df0b3720e5c6841cd6f3c1905a658906ece
foued-ben <foued.bzd@hotmail.fr>
2017-12-05 15:44:09 +0100

    Anne : ajout voyage

---
+++ ProjetBoVoyages/src/main/java/fr/adaming/controllers/VoyageController.java
@@ -0,0 +1,40 @@
+package fr.adaming.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.adaming.model.Voyage;
+import fr.adaming.service.IVoyageService;
+
+@Controller
+@Scope("session")
+@RequestMapping("/voyage")
+public class VoyageController {
+	@Autowired
+	IVoyageService voyageService;
+	
+	public void setVoyageService(IVoyageService voyageService) {
+		this.voyageService = voyageService;
+	}
+
+	@RequestMapping(value="/afficheAjout", method=RequestMethod.GET)
+	public ModelAndView afficheFormAjout(){
+		return new ModelAndView("ajoutVoyage", "voyageAjoute", new Voyage());
+	}
+	
+	@RequestMapping(value="/ajouteVoyage", method=RequestMethod.POST)
+	public String soumettreFormAjout(Model modele, @ModelAttribute("voyageAjoute") Voyage voyage){
+		Voyage vOut=voyageService.ajoutVoyage(voyage);
+		if(vOut!=null){
+			
+		}
+		return null;
+	}
+	
+}
