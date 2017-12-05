84356c81ddda160be609ef761b9b7ed94a02ad77
inti <inti@inti0226>
2017-12-05 14:56:36 +0100

    Hadrien: test affichage index

--- ProjetBoVoyages/src/main/java/fr/adaming/controllers/AgentController.java
+++ ProjetBoVoyages/src/main/java/fr/adaming/controllers/AgentController.java
@@ -3,10 +3,17 @@ package fr.adaming.controllers;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Scope("session")
 @RequestMapping("/agent")
 public class AgentController {
 
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public ModelAndView afficherRecapEtudiant() {
+		System.out.println("Test");
+		return new ModelAndView("acueil");
+	}
 }
--- ProjetBoVoyages/src/main/webapp/index.jsp
+++ ProjetBoVoyages/src/main/webapp/index.jsp
@@ -4,9 +4,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Test</title>
-</head>
+<title>Index</title>
 <body>
-	<h1>Test</h1>
+	<h1>Index</h1>
+<%-- 	<% response.sendRedirect("accueil"); %> --%>
 </body>
 </html>
