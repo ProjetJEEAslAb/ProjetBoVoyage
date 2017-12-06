41fdceb4e741f658c75fc252320d9a8ac1304e6d
inti <inti@inti0226>
2017-12-06 11:01:14 +0100

    Hadrien: correction annotation Table

--- ProjetBoVoyages/src/main/java/fr/adaming/model/Agent.java
+++ ProjetBoVoyages/src/main/java/fr/adaming/model/Agent.java
@@ -8,8 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="agents")
 public class Agent implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
--- ProjetBoVoyages/src/main/java/fr/adaming/model/Role.java
+++ ProjetBoVoyages/src/main/java/fr/adaming/model/Role.java
@@ -12,6 +12,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
+@Table(name="roles")
 public class Role implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
