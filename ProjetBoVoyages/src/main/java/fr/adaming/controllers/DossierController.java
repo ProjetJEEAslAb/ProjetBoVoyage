54f40bf2876c8958c2cc43b2c46b91631f0076cd
inti0449 <inti0449@192.168.1.165>
2017-12-06 15:48:15 +0100

    David: presque toutes les vues Dossier

---
+++ ProjetBoVoyages/src/main/java/fr/adaming/controllers/DossierController.java
@@ -0,0 +1,126 @@
+package fr.adaming.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import fr.adaming.model.Dossier;
+import fr.adaming.service.IDossierService;
+
+@Controller
+@Scope("session")
+@RequestMapping("/dossier")
+public class DossierController {
+
+	// Injection de dépendance
+	@Autowired
+	IDossierService dossierService;
+
+	// Méthodes controller
+
+	// Affichage de la liste des dossiers
+	@RequestMapping(value = "/listeDossiers", method = RequestMethod.GET)
+	public ModelAndView afficherListeDossiers() {
+
+		// Récupération de la liste
+		List<Dossier> listeDossier = dossierService.getAllDossiers();
+
+		return new ModelAndView("dossier/listeDossier", "listeDossiers", listeDossier);
+
+	}
+
+	// Formulaire d'ajout de dossier
+	@RequestMapping(value = "/afficheAjout", method = RequestMethod.GET)
+	public ModelAndView afficherFormAjout() {
+		return new ModelAndView("dossier/ajoutDossier", "dossierAjout", new Dossier());
+	}
+
+	@RequestMapping(value = "/ajoutDoss", method = RequestMethod.POST)
+	public String soumettreFormAjout(RedirectAttributes redirectAttributes, Model modele,
+			@ModelAttribute("dossierAjout") Dossier dossier, BindingResult result) {
+		Dossier dosOut = dossierService.addDossier(dossier);
+
+		if (result.hasErrors()) {
+			return "dossier/ajoutDossier";
+		} else if (dosOut.getId() != 0) {
+			// Actualiser la liste de dossiers
+
+			List<Dossier> listeOut = dossierService.getAllDossiers();
+			modele.addAttribute("listeDossiers", listeOut);
+
+			return "dossier/listeDossier";
+		} else {
+			
+			redirectAttributes.addFlashAttribute("message", "Le dossier n'a pas pu être ajouté");
+			return "redirect:afficheAjout";
+		}
+	}
+
+	// Formulaire de modification d'un dossier
+	@RequestMapping(value = "/afficheModif", method = RequestMethod.GET)
+	public ModelAndView afficheFormModif() {
+
+		return new ModelAndView("dossier/modifDossier", "dossierModif", new Dossier());
+
+	}
+
+	@RequestMapping(value="/modifDoss", method=RequestMethod.POST)
+	public String soumettreFormModif(Model modele, @ModelAttribute("dossierModif") Dossier dossier,
+			BindingResult result) {
+
+		dossierService.updateDossier(dossier);
+		
+		if (result.hasErrors()){
+			return "dossier/modifDossier";
+		} else {
+			
+			// Actualiser la liste de dossiers
+			List<Dossier> listeOut = dossierService.getAllDossiers();
+			modele.addAttribute("listeDossiers", listeOut);
+			
+			return "dossier/listeDossier";
+			
+		}
+
+	}
+	
+	
+	// Formulaire de suppression d'un dossier
+	@RequestMapping(value="/afficheSuppr", method=RequestMethod.GET)
+	public ModelAndView afficheFormSuppr() {
+		
+		return new ModelAndView("dossier/supprDossier", "dossierSuppr", new Dossier());
+		
+	}
+	
+	@RequestMapping (value = "/supprDoss", method=RequestMethod.POST)
+	public String soumettreFormSuppr(Model modele, @ModelAttribute("dossierSuppr") Dossier dossier, BindingResult result) {
+		
+		// Appel de la méthode Service
+		dossierService.deleteDossier(dossier);
+		
+		if (result.hasErrors()){
+			return "dossier/supprDossier";
+		} else {
+			
+			// Actualiser la liste de dossiers
+			List<Dossier> listeOut = dossierService.getAllDossiers();
+			modele.addAttribute("listeDossiers", listeOut);
+			
+			return "dossier/listeDossier";
+			
+		}
+		
+	}
+
+}
---
+++ ProjetBoVoyages/src/main/webapp/WEB-INF/pages/dossier/listeDossier.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+<link rel="stylesheet"
+	href="assets/libs/bootstrap-3.3.7/css/bootstrap.css">
+<link rel="stylesheet"
+	href="https://bootswatch.com/3/superhero/bootstrap.min.css">
+<script type="text/javascript" src="assets/libs/jquery-3.2.1.js"></script>
+<script type="text/javascript"
+	src="assets/libs/bootstrap-3.3.7/js/bootstrap.js"></script>
+
+<title>Liste des dossiers</title>
+</head>
+<body>
+
+<h1 style="color: blue; text-align: center">Liste des Dossiers</h1>
+	<div align="center">
+		<table class="table table-bordered">
+			<tr>
+				<th>ID</th>
+				<th>Statut</th>
+				<th>Assurance</th>
+				<th>Code CB</th>
+				<th>operations</th>
+			</tr>
+
+			<c:forEach var="dossier" items="${listeDossiers}">
+				<tr>
+					<td>${dossier.id}</td>
+					<td>${dossier.statut}</td>
+					<td>${dossier.assurance}</td>
+					<td>${dossier.codeCB}</td>
+					
+					<td><a
+						href="${pageContext.request.contextPath}/voyageur/SupprimeViaLien?pId=${etudiant.id}">supprimer</a>|<a
+						href="${pageContext.request.contextPath}/etudiant/ModifViaLien${etudiant.id}">modifier</a></td>
+				</tr>
+			</c:forEach>
+		</table>
+	</div>
+
+</body>
+</html>
